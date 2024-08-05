c8093575dad17994e642ca0b67467a5650a5c3d5
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-05 12:58:23 +0530

    state update with only atom(generally do)

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,46 +2,46 @@ import React, { useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import {
   countAtom,
-  decrementBy100Selector,
-  decrementSelector,
-  incrementBy100Selector,
-  incrementSelector,
+  //   decrementBy100Selector,
+  //   decrementSelector,
+  //   incrementBy100Selector,
+  //   incrementSelector,
 } from "../store/atoms/count";
 
 function Counter() {
   const [count, setCount] = useRecoilState(countAtom);
-  const increment = useRecoilValue(incrementSelector);
-  const decrement = useRecoilValue(decrementSelector);
-  const incrementBy100 = useRecoilValue(incrementBy100Selector);
-  const decrementBy100 = useRecoilValue(decrementBy100Selector);
+  //   const increment = useRecoilValue(incrementSelector);
+  //   const decrement = useRecoilValue(decrementSelector);
+  //   const incrementBy100 = useRecoilValue(incrementBy100Selector);
+  //   const decrementBy100 = useRecoilValue(decrementBy100Selector);
 
   return (
     <div>
       <h1>{count}</h1>
       <button
         onClick={() => {
-          setCount(increment);
+          setCount(count + 1);
         }}
       >
         +
       </button>
       <button
         onClick={() => {
-          setCount(decrement);
+          setCount(count - 1);
         }}
       >
         -
       </button>
       <button
         onClick={() => {
-          setCount(incrementBy100);
+          setCount(count + 100);
         }}
       >
         +100
       </button>
       <button
         onClick={() => {
-          setCount(decrementBy100);
+          setCount(count - 100);
         }}
       >
         -100
--- src/store/atoms/count.jsx
+++ src/store/atoms/count.jsx
@@ -5,34 +5,34 @@ export const countAtom = atom({
   default: 0,
 });
 
-export const incrementSelector = selector({
-  key: "incrementSelector",
-  get: ({ get }) => {
-    const count = get(countAtom);
-    return count + 1;
-  },
-});
+// export const incrementSelector = selector({
+//   key: "incrementSelector",
+//   get: ({ get }) => {
+//     const count = get(countAtom);
+//     return count + 1;
+//   },
+// });
 
-export const decrementSelector = selector({
-  key: "decrementSelector",
-  get: ({ get }) => {
-    const count = get(countAtom);
-    return count - 1;
-  },
-});
+// export const decrementSelector = selector({
+//   key: "decrementSelector",
+//   get: ({ get }) => {
+//     const count = get(countAtom);
+//     return count - 1;
+//   },
+// });
 
-export const incrementBy100Selector = selector({
-  key: "incrementBy100Selector",
-  get: ({ get }) => {
-    const count = get(countAtom);
-    return count + 100;
-  },
-});
+// export const incrementBy100Selector = selector({
+//   key: "incrementBy100Selector",
+//   get: ({ get }) => {
+//     const count = get(countAtom);
+//     return count + 100;
+//   },
+// });
 
-export const decrementBy100Selector = selector({
-  key: "decrementBy100Selector",
-  get: ({ get }) => {
-    const count = get(countAtom);
-    return count - 100;
-  },
-});
+// export const decrementBy100Selector = selector({
+//   key: "decrementBy100Selector",
+//   get: ({ get }) => {
+//     const count = get(countAtom);
+//     return count - 100;
+//   },
+// });
